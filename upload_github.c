896725b99a591a23c70342fa113797ad9ee0375b
Iftekhar03 <97311559+Iftekhar03@users.noreply.github.com>
2022-11-03 10:08:23 +0600

    Add files via upload

---
+++ upload_github.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,n,x,y,z,count,i,j,a,b,c;
+	scanf("%d",&t);
+	while(t--)
+	{
+	  scanf("%d",&x);
+      if(x<=50){
+      printf("LEFT\n");
+      }
+      else{
+        printf("RIGHT\n");
+      }
+	}
+	return 0;
+}
